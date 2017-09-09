c8cd9f96047b7d8f16879924e46e025e11eb04dc
paritosh_pundir <0p0a0r0i@gmail.com>
2017-09-10 00:02:29 +0530

    Basic styling and layout refactoring

--- src/components/Example.jsx
+++ src/components/Example.jsx
@@ -4,13 +4,25 @@ import ChatBot from '../../bot/index';
 const steps = [
   {
     id: '1',
-    message: 'Hello World',
-    end: true,
+    message: 'What is your name?',
+    trigger: '2',
+  },
+  {
+    id: '2',
+    user: true,
+    trigger: '3',
+  },
+  {
+    id: '3',
+    message: 'Hi {previousValue}, nice to meet you!',
+    trigger: '1',
   },
 ];
 
 const ThemedExample = () => (
-  <ChatBot steps={steps} />
+  <ChatBot
+    steps={steps}
+  />
 );
 
 export default ThemedExample;
